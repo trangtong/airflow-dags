4feb160c901b05be4432ab61098dc664d38b6bbe
trangtong <50126855+trangtong@users.noreply.github.com>
2024-07-04 16:47:44 +0700

    Update dbt_advanced_utility.py

--- airflow-dbt-demo-master/dags/dbt_advanced_utility.py
+++ airflow-dbt-demo-master/dags/dbt_advanced_utility.py
@@ -15,7 +15,7 @@ from dbt_dag_parser import DbtDagParser
 
 # We're hardcoding these values here for the purpose of the demo, but in a production environment these
 # would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "/usr/local/airflow/dbt"
+DBT_PROJECT_DIR = "../dbt"
 DBT_GLOBAL_CLI_FLAGS = "--no-write-json"
 DBT_TARGET = "dev"
 DBT_TAG = "tag_staging"
@@ -30,23 +30,23 @@ with DAG(
     doc_md=__doc__
 ) as dag:
 
-    start_dummy = DummyOperator(task_id="start")
-    # We're using the dbt seed command here to populate the database for the purpose of this demo
-    dbt_seed = BashOperator(
-        task_id="dbt_seed",
-        bash_command=(
-            f"dbt {DBT_GLOBAL_CLI_FLAGS} seed "
-            f"--profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}"
-        ),
-        env={
-            "DBT_USER": "{{ conn.postgres.login }}",
-            "DBT_ENV_SECRET_PASSWORD": "{{ conn.postgres.password }}",
-            "DBT_HOST": "{{ conn.postgres.host }}",
-            "DBT_SCHEMA": "{{ conn.postgres.schema }}",
-            "DBT_PORT": "{{ conn.postgres.port }}",
-        },
-    )
-    end_dummy = DummyOperator(task_id="end")
+    # start_dummy = DummyOperator(task_id="start")
+    # # We're using the dbt seed command here to populate the database for the purpose of this demo
+    # dbt_seed = BashOperator(
+    #     task_id="dbt_seed",
+    #     bash_command=(
+    #         f"dbt {DBT_GLOBAL_CLI_FLAGS} seed "
+    #         f"--profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}"
+    #     ),
+    #     env={
+    #         "DBT_USER": "{{ conn.postgres.login }}",
+    #         "DBT_ENV_SECRET_PASSWORD": "{{ conn.postgres.password }}",
+    #         "DBT_HOST": "{{ conn.postgres.host }}",
+    #         "DBT_SCHEMA": "{{ conn.postgres.schema }}",
+    #         "DBT_PORT": "{{ conn.postgres.port }}",
+    #     },
+    # )
+    # end_dummy = DummyOperator(task_id="end")
 
     # The parser parses out a dbt manifest.json file and dynamically creates tasks for "dbt run" and "dbt test"
     # commands for each individual model. It groups them into task groups which we can retrieve and use in the DAG.
