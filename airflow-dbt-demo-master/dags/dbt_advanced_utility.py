a8295325bea551f72016299a9a447bc9afaf342c
trangtong <50126855+trangtong@users.noreply.github.com>
2024-07-04 16:50:01 +0700

    Update dbt_advanced_utility.py

--- airflow-dbt-demo-master/dags/dbt_advanced_utility.py
+++ airflow-dbt-demo-master/dags/dbt_advanced_utility.py
@@ -4,6 +4,7 @@ Shows how to parse a dbt manifest file to "explode" the dbt DAG into Airflow
 Each dbt model is run as a bash command.
 """
 import sys
+import os
 sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
 from pendulum import datetime
